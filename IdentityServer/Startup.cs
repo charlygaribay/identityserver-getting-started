f35ccc2c61ca127fbc1aa11c5ab7f80f4e5a4a7b
Charly Garibay <jcgaribayr@outlook.com>
2018-07-17 03:27:34 -0500

    Defining the api. Defining the client. Configure identity server. Adding an api. Creating the client.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -25,7 +25,9 @@ namespace IdentityServer
         {
             services
                 .AddIdentityServer()
-                .AddDeveloperSigningCredential();
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
