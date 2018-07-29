60fd9cd7a8093031f6cc6609c2424729b6d2392c
Charly Garibay <jcgaribayr@outlook.com>
2018-07-28 19:47:23 -0500

    Add Tasks controller

---
+++ Identity/Controllers/Api/TasksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.ApiModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Events")]
+    public class EventsController : Controller
+    {
+        public async Task<ActionResult> GetEvents()
+        {
+            await Task.Delay(10);
+
+            return Ok(new List<TaskApiModel>()
+            {
+                new TaskApiModel
+                {
+                    Id = 123,
+                    Title = "Hola",
+                    Description = "Hello"
+                }
+            });
+        }
+    }
+}
